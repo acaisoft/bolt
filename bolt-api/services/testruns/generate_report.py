a2f85927bf2f764d2808963a5f94831d8d8dfcc9
Jerzy Goebel <jerzy.goebel@acaisoft.com>
2022-08-15 02:36:59 +0200

    - changing single quotes to double quotes - adding missing try/except blocks

--- bolt-api/services/testruns/generate_report.py
+++ bolt-api/services/testruns/generate_report.py
@@ -11,6 +11,9 @@ from services.logger import setup_custom_logger
 logger = setup_custom_logger(__name__)
 
 
+class ReportGenerationException(Exception):
+    ...
+
 class Status(Enum):
     STATUS_ERROR = 'error'
     STATUS_NOT_GENERATED = 'not_generated'
@@ -25,7 +28,7 @@ def generate_report(app_config, execution_id: str) -> str:
     logger.info("Report Generation")
 
     client = StorageClient(app_config)
-    file_name = f'reports/{execution_id}.pdf'
+    file_name = f"reports/{execution_id}.pdf"
 
     response = hce(app_config, '''query ($executionId:uuid!) {
         execution(
@@ -36,7 +39,12 @@ def generate_report(app_config, execution_id: str) -> str:
     }''', {
         "executionId": str(execution_id)
     })
-    report_status = response["execution"][0]["report"]
+    try:
+        report_status = response["execution"][0]["report"]
+    except (KeyError, IndexError) as ex:
+        logger.error(f"Execution identified by {execution_id} does not exist, "
+                     f"or read permissions on table 'execution' are missing.")
+        raise ReportGenerationException(f"Execution {execution_id} not found.")
     if report_status == Status.STATUS_READY.value:
         if client.file_exists(file_name):
             return client.generate_download_url(blob_name=file_name)
@@ -46,7 +54,12 @@ def generate_report(app_config, execution_id: str) -> str:
         trigger_report_generator(client, file_name, execution_id, app_config)
 
 def trigger_report_generator(client: StorageClient, file_name: str, execution_id: str, app_config):
-    url = client.generate_upload_url(blob_name=file_name)
+    try:
+        url = client.generate_upload_url(blob_name=file_name)
+    except Exception as ex:
+        logger.error(f"Failure during upload url generation: {ex}")
+        raise ReportGenerationException(ex)
+
     hce(app_config, '''
         mutation update($id:uuid!, $status:String) {
           update_execution_by_pk(pk_columns: {id: $id}, _set: {report: $status}) {
@@ -56,33 +69,35 @@ def trigger_report_generator(client: StorageClient, file_name: str, execution_id
         "id": str(execution_id),
         "status": "generating"
     })
+
     try:
         hasura_token, _ = generate_hasura_token(app_config, const.ROLE_READER)
         workflow_data = {
-            'tenant_id': '1',
-            'project_id': "",
-            'repository_url': "",
-            'branch': "",
-            'execution_id': str(execution_id),
-            'auth_token': hasura_token,
-            'duration_seconds': 123,
-            'job_pre_start': None,
-            'job_load_tests': None,
-            'job_monitoring': None,
-            'job_post_stop': None,
-            'job_report': {
+            "tenant_id": "1",
+            "project_id": "",
+            "repository_url": "",
+            "branch": "",
+            "execution_id": str(execution_id),
+            "auth_token": hasura_token,
+            "duration_seconds": 123,
+            "job_pre_start": None,
+            "job_load_tests": None,
+            "job_monitoring": None,
+            "job_post_stop": None,
+            "job_report": {
                 "env_vars": {
                     "UPLOAD_URL": url
                 }
             },
-            'no_cache': True,
+            "no_cache": True,
         }
     except Exception as ex:
-        logger.error(f'Failure during Argo workflow data preparation: {ex}')
-        raise ex
+        logger.error(f"Failure during Argo workflow data preparation: {ex}")
+        raise ReportGenerationException(ex)
+
     try:
         workflow = WorkflowsResource(KubernetesService(app_config))
         workflow_state = workflow.generate_report(workflow_data)
     except Exception as ex:
-        logger.error(f'Failure during Argo workflow triggering: {ex}')
-        raise ex
+        logger.error(f"Failure during Argo workflow triggering: {ex}")
+        raise ReportGenerationException(ex)
