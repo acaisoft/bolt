be61f3cd8de19a6ab7c0550ca8f61920f8a521f3
Jerzy Goebel <jerzy.goebel@acaisoft.com>
2022-11-21 18:32:12 +0100

    - (migrations) add cpu_warning field and critical permissions to it - (front) add CPUWarningBadge component - (front) tweak executions subscription so that cpu_warning field is queried as well - (front) add CPU badge to Details page
    task: ACBT-729

---
+++ bolt-api/subsystems/hasura/migrations/default/1669047367727_add_cpu_warning/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."execution" add column "cpu_warning" boolean
+--  not null default 'false';
